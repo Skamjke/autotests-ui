34bf0544763070cdc4b8d2dbdbb70ecf29cdc420
Skamjke <freezy512@gmail.com>
2026-06-05 16:50:19 +0700

    Работа с авторизацией в Playwright

---
+++ playwright_courses.py
@@ -0,0 +1,47 @@
+from time import sleep
+
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    reg_email_input = page.get_by_test_id('registration-form-email-input').locator('//div//input')
+    reg_email_input.fill('user.name@gmail.com')
+
+    reg_username_input = page.get_by_test_id('registration-form-username-input').locator('//div//input')
+    reg_username_input.fill('username')
+
+    reg_password_input = page.get_by_test_id('registration-form-password-input').locator('//div//input')
+    reg_password_input.fill('password')
+
+    reg_button = page.get_by_test_id('registration-page-registration-button')
+    reg_button.click()
+
+    context.storage_state(path='browser-state.json')
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    list_toolbar_text = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(list_toolbar_text).to_have_text('Courses')
+
+    icon_folder = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon_folder).to_be_visible()
+
+    list_empty_text = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(list_empty_text).to_have_text('There is no results')
+
+    list_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(list_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+
+
+
