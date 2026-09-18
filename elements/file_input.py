b76b1981845d793b2e07257c1c41b8902cee6780
Skamjke <freezy512@gmail.com>
2026-09-18 19:29:57 +0700

    Логирование взаимодействий со страницей

--- elements/file_input.py
+++ elements/file_input.py
@@ -3,6 +3,11 @@ from playwright.sync_api import Locator
 from elements.base_element import BaseElement
 import allure
 
+from tools.logger import get_logger
+
+logger = get_logger("FILE_INPUT_ELEMENT")
+
+
 class FileInput(BaseElement):
 
     @property
@@ -10,6 +15,8 @@ class FileInput(BaseElement):
         return 'file input'
 
     def set_input_files(self, file_path: str,nth: int = 0, **kwargs):
-        with allure.step(f'Set file "{file_path}" to the {self.type_of} "{self.name}"'):
+        step = f'Set file "{file_path}" to the {self.type_of} "{self.name}"'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.set_input_files(file_path)
