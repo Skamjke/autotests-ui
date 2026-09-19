683556bdd325037ebcc67d1bb4b4c8055ce995cf
Skamjke <freezy512@gmail.com>
2026-09-19 19:11:21 +0700

    Запуск автотестов на GitHub Actions

--- playwright_courses.py
+++ playwright_courses.py
@@ -29,6 +29,7 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
 
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
 
