edafe4cf514babb790e222afa3feda2d09111370
Skamjke <freezy512@gmail.com>
2026-09-19 18:28:15 +0700

    Запуск автотестов на GitHub Actions

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -7,6 +7,7 @@ with sync_playwright() as playwright:
     page = browser.new_page()
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
+
     input_email = page.get_by_test_id('login-form-email-input').locator('//div//input')
     input_email.fill('user.name@gmail.com')
 
