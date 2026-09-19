210814cae1f4aa8a344873313a0e31d2c876e9f8
Skamjke <freezy512@gmail.com>
2026-09-19 18:11:52 +0700

    Подготовка проекта к запуску на CI

--- playwright_js.py
+++ playwright_js.py
@@ -2,6 +2,7 @@ from time import sleep
 
 from playwright.sync_api import sync_playwright, expect
 
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
