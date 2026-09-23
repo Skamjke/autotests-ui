375c6a189e81f1ae38364b7061313739df06106f
Skamjke <freezy512@gmail.com>
2026-09-23 17:32:20 +0700

    Практикуемся в интеграции покрытия в UI тесты

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Page, Locator, expect
 import allure
-
+from elements.ui_coverage import tracker
 from tools.logger import get_logger
-
+from ui_coverage_tool import ActionType, SelectorType
 logger = get_logger("BASE_ELEMENT")
 
 class BaseElement:
@@ -21,12 +21,23 @@ class BaseElement:
             logger.info(step)
             return self.page.get_by_test_id(locator).nth(nth)
 
+    def get_raw_locator(self, nth:int = 0, **kwargs) -> str:
+        return f"//*[@data-testid='{self.locator.format(**kwargs)}'][{nth + 1}]"
+
+    def track_coverage(self, action_type: ActionType, nth:int = 0, **kwargs):
+        tracker.track_coverage(
+            selector=self.get_raw_locator(nth, **kwargs),
+            action_type=action_type,
+            selector_type=SelectorType.XPATH
+        )
+
     def click(self,nth: int = 0, **kwargs):
         step = f'Clicking on {self.type_of} "{self.name}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.click()
+        self.track_coverage(ActionType.CLICK, nth=nth, **kwargs)
 
     def check_visible(self,nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is visible'
@@ -34,6 +45,7 @@ class BaseElement:
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_visible()
+        self.track_coverage(ActionType.VISIBLE, nth=nth, **kwargs)
 
     def have_text(self, text: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
@@ -41,3 +53,5 @@ class BaseElement:
             locator = self.get_locator(nth,**kwargs)
             logger.info(step)
             expect(locator).to_have_text(text)
+
+        self.track_coverage(ActionType.TEXT, nth=nth, **kwargs)
